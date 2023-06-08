1d944327b6693bb2fa9b8a8718e46d970462f485
Geistsoft <123596322+NassimMansouri@users.noreply.github.com>
2023-06-08 20:20:16 +0100

    Update main.py
    -Fixed script stoppage once song is paused 
    -Fixed script stoppage once a song without lyrics is played

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ from requests import get
 from requests import patch
 import os
 import emoji
+import time
 
 # Your discord token
 discord_token = ''
-
+error_message = '{"error":true,"message":"lyrics for this track is not available on spotify!"}'
 scope = "user-read-currently-playing"
 url_discord = 'https://discordapp.com/api/v6/users/@me/settings'
 headers_discord = {
@@ -23,42 +24,42 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 results = sp.currently_playing()
 print(results['item']['name'])
 curr_track_id = results['item']['id']
-lyrics = get(f"https://spotify-lyric-api.herokuapp.com/?trackid={curr_track_id}").content
-lyrics = json.loads(lyrics)
-while lyrics['error'] :
-	lyrics = get(f"https://spotify-lyric-api.herokuapp.com/?trackid={curr_track_id}").content
-	lyrics = json.loads(lyrics)
-print(len(lyrics['lines']))
+def get_lyrics(track_id):
+	lyrics = get(f"https://spotify-lyric-api.herokuapp.com/?trackid={track_id}").content
+	if lyrics.decode() != error_message : 
+		lyrics = json.loads(lyrics)
+		while lyrics['error'] :
+			lyrics = get(f"https://spotify-lyric-api.herokuapp.com/?trackid={track_id}").content
+			lyrics = json.loads(lyrics)
+		return lyrics
+	return ' '
+lyrics = get_lyrics(curr_track_id)
 previous_time = 0
 current_top = 0 
 current_bottom = 0
 
 
-def get_lyrics(track_id):
-	lyrics = get(f"https://spotify-lyric-api.herokuapp.com/?trackid={track_id}").content
-	lyrics = json.loads(lyrics)
-	while lyrics['error'] :
-		lyrics = get(f"https://spotify-lyric-api.herokuapp.com/?trackid={track_id}").content
-		lyrics = json.loads(lyrics)
-	return lyrics
+while True : 
+	while results['is_playing'] and lyrics != ' ':
+		results = sp.currently_playing()
+		current_time = int(results['progress_ms'])
+		if results['timestamp'] > 0 :
+			lyrics = get_lyrics(results['item']['id'])
+		if lyrics != ' ' : 
+			for i in range(len(lyrics['lines']) - 1):
+				if current_time > int(lyrics['lines'][i]['startTimeMs']) and current_time < int(lyrics['lines'][i + 1]['startTimeMs']) and current_time > previous_time :
+					print(lyrics['lines'][i]['words'])
+					previous_time = int(lyrics['lines'][i + 1]['startTimeMs'])
+					curr_lyrics = emoji.demojize(lyrics['lines'][i]['words'])
 
+					current_top = int(lyrics['lines'][i + 1]['startTimeMs'])
+					current_bottom = int(lyrics['lines'][i]['startTimeMs'])
+					patch(url_discord, json={'custom_status':{'text': f'{curr_lyrics}','expires_at': None}},headers=headers_discord)
 
-while results['is_playing']:
+		if current_time < current_bottom or current_time > current_top:
+			previous_time = 0
+	time.sleep(1)
 	results = sp.currently_playing()
-	current_time = int(results['progress_ms'])
 	if results['timestamp'] > 0 :
 		lyrics = get_lyrics(results['item']['id'])
-	for i in range(len(lyrics['lines']) - 1):
-		if current_time > int(lyrics['lines'][i]['startTimeMs']) and current_time < int(lyrics['lines'][i + 1]['startTimeMs']) and current_time > previous_time :
-			print(lyrics['lines'][i]['words'])
-			previous_time = int(lyrics['lines'][i + 1]['startTimeMs'])
-			curr_lyrics = emoji.demojize(lyrics['lines'][i]['words'])
-			# with open('lyrics.txt', 'w',encoding='utf8') as f:
-				# f.write(curr_lyrics)
-			current_top = int(lyrics['lines'][i + 1]['startTimeMs'])
-			current_bottom = int(lyrics['lines'][i]['startTimeMs'])
-			patch(url_discord, json={'custom_status':{'text': f'{curr_lyrics}','expires_at': None}},headers=headers_discord)
-
-	if current_time < current_bottom or current_time > current_top:
-		previous_time = 0
 
