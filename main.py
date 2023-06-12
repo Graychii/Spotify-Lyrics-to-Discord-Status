137d2a832a70b94fc244439660cd21558c2eb80b
Geistsoft <123596322+NassimMansouri@users.noreply.github.com>
2023-06-12 23:38:44 +0100

    Added censorship option

--- main.py
+++ main.py
@@ -7,12 +7,18 @@ import os
 import emoji
 import time
 import random
-
-
-
-
 # Your discord token
-discord_token = ''
+discord_token = 'MzgzNzM1NDkxMDc1NTA2MTc4.GgaygM.ZOup_LQevncn1TN4_QXUNlClx4-GETwgKQ7ZPE'
+
+#Censored words
+censored_words = (open('censored_words.txt').read()).split(',')
+censor = False
+
+
+
+
+
+
 
 
 
@@ -23,6 +29,23 @@ if answer.upper() == 'Y' :
 	discord_emojis = ['🤩','🎵', '🔥','🥰', '😇', '💀','☠️', '👻', '💪', '💅', '💋', '👑', '💍', '💄', '🤰🏻', '💥', '☄️', '⚡️', '✨', '🌟', '⭐️', '💫', '🪐', '🔥', '🌪', '☀️','🌩', '❤️', '💔', '💞']
 else : 
 	discord_emojis = [' ']
+
+
+answer = str(input('Would you like to enable censorship of bad words ? Y/n : '))
+if answer.upper() == 'Y':
+	print('Note : Censored words are added manually by you within the censored_words.txt file')
+	censor = True
+
+
+def check_for_censorship(lyrics):
+	for word in censored_words : 
+		if word.upper() in lyrics.upper():
+			word = word.lower()
+			lyrics = lyrics.lower()
+			lyrics = lyrics.replace(word,'****')
+	return lyrics
+
+
 error_message = '{"error":true,"message":"lyrics for this track is not available on spotify!"}'
 scope = "user-read-currently-playing"
 url_discord = 'https://discordapp.com/api/v6/users/@me/settings'
@@ -63,17 +86,21 @@ while True :
 			current_time = int(results['progress_ms'])
 			if results['timestamp'] > 0 :
 				lyrics = get_lyrics(results['item']['id'])
+
 			if lyrics != ' ' : 
 				for i in range(len(lyrics['lines']) - 1):
 					if current_time > int(lyrics['lines'][i]['startTimeMs']) and current_time < int(lyrics['lines'][i + 1]['startTimeMs']) and current_time > previous_time :
 						print(lyrics['lines'][i]['words'])
 						previous_time = int(lyrics['lines'][i + 1]['startTimeMs'])
 						curr_lyrics = emoji.demojize(lyrics['lines'][i]['words'])
+						if censor : 
+							curr_lyrics = check_for_censorship(curr_lyrics)
+							print(curr_lyrics)
 
 						current_top = int(lyrics['lines'][i + 1]['startTimeMs'])
 						current_bottom = int(lyrics['lines'][i]['startTimeMs'])
 
-						patch(url_discord, json={'custom_status':{'text': f'{random.choice(discord_emojis)} {curr_lyrics} {random.choice(discord_emojis)}','expires_at': None}},headers=headers_discord)
+						# patch(url_discord, json={'custom_status':{'text': f'{random.choice(discord_emojis)} {curr_lyrics} {random.choice(discord_emojis)}','expires_at': None}},headers=headers_discord)
 
 			if current_time < current_bottom or current_time > current_top:
 				previous_time = 0
@@ -86,6 +113,8 @@ while True :
 		results = sp.currently_playing()
 		if results['timestamp'] > 0 :
 			lyrics = get_lyrics(results['item']['id'])
+			if censor : 
+				lyrics = check_for_censorship(lyrics)
 	except : 
 		print('Connection lost')
 
